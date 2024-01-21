7a53620f589f2439a0412ecae3a9448af41de255
Biagio Rosario Greco <32863770+birg81@users.noreply.github.com>
2024-01-21 17:50:24 +0100

    Delete AS-2023-2024/5X-TPSIT/v2.0-orm directory

--- AS-2023-2024/5X-TPSIT/v2.0-orm/db/create_db.py
+++
@@ -1,3 +0,0 @@
-from db.database import Base, engine
-from models.PersonModel import PersonModel
-Base.metadata.create_all(engine)
--- AS-2023-2024/5X-TPSIT/v2.0-orm/entity/Person.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-from pydantic.types import PositiveInt
-
-class Person(BaseModel):
-	id: PositiveInt
-	firstname: str = Field(min_length = 2, max_length = 48, default = "(firstname)")
-	lastname: str = Field(min_length = 2, max_length = 48, default = "(lastname)")
-	age: PositiveInt
